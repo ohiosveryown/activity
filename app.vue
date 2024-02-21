95938d6e5ab0727b61b3777da0eed91be2bfcee0
ohiosveryown <matt@ohiosveryown.co>
2024-02-20 21:12:12 -0500

    var setup

--- app.vue
+++ app.vue
@@ -2,6 +2,8 @@
   <div>
     <!-- <rm /> -->
 
+    <h1>{{ myVariable }}</h1>
+
     <ul>
       <li v-for="(activity, x) in activities" :key="x">
         <h2>{{ activity.date }}</h2>
@@ -25,34 +27,50 @@
 
 <script>
   export default {
-    data: () => ({
-      activities: [
-        // { name: "Davido", genre: "afrobeats", country: "Nigeria" },
-        // { name: "Burna Boy", genre: "afrobeats", country: "Nigeria" },
-        // { name: "AKA", genre: "hiphop", country: "South-Africa" },
-        // { name: "Sarkodie", genre: "hiphop", country: "Ghana" },
-        // { name: "Stormzy", genre: "hiphop", country: "United Kingdom" },
-        // { name: "Lil Nas", genre: "Country", country: "United States" },
-        // { name: "Nasty C", genre: "hiphop", country: "South-Africa" },
-        // { name: "Shatta-walle", genre: "Reagae", country: "Ghana" },
-        // { name: "Khalid", genre: "pop", country: "United States" },
-        // { name: "ed-Sheeran", genre: "pop", country: "United Kingdom" },
+    // setup() {
+    //   const myVariable = ref("Initial value")
+    //   return { myVariable }
+    // },
 
-        {
-          date: "Jan 13",
-          type: "🏃🏼‍♂️",
-          distance: "2.00 mi",
-          time: "9:26 / mi",
-          img: "/img/map@2x.png",
-        },
-        {
-          date: "Jan 28",
-          type: "🏀",
-          distance: "",
-          time: "45 min",
-          img: "/img/gym@2x.png",
-        },
-      ],
-    }),
+    // data: () => ({
+    //   activities: [
+    //     {
+    //       date: "Jan 13",
+    //       type: "🏃🏼‍♂️",
+    //       distance: "2.00 mi",
+    //       time: "9:26 / mi",
+    //       img: "/img/map@2x.png",
+    //     },
+    //     {
+    //       date: "Jan 28",
+    //       type: "🏀",
+    //       distance: "",
+    //       time: "45 min",
+    //       img: "/img/gym@2x.png",
+    //     },
+    //   ],
+    // }),
+
+    data() {
+      return {
+        myVariable: "Hellooooo",
+        activities: [
+          {
+            date: "Jan 13",
+            type: "🏃🏼‍♂️",
+            distance: "2.00 mi",
+            time: "9:26 / mi",
+            img: "/img/map@2x.png",
+          },
+          {
+            date: "Jan 28",
+            type: "🏀",
+            distance: "",
+            time: "45 min",
+            img: "/img/gym@2x.png",
+          },
+        ],
+      }
+    },
   }
 </script>
