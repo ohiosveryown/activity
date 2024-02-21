f40ebaaf7e652de6e3879ab1e070e676c0946ab8
ohiosveryown <matt@ohiosveryown.co>
2024-02-21 18:51:04 -0500

    before reset

--- app.vue
+++ app.vue
@@ -1,27 +1,94 @@
 <template>
-  <div>
-    <!-- <rm /> -->
+  <!-- <rm /> -->
+  <!-- <dates /> -->
 
-    <h1>{{ myVariable }}</h1>
-
-    <ul>
-      <li v-for="(activity, x) in activities" :key="x">
-        <h2>{{ activity.date }}</h2>
-        <h3>{{ activity.type }}</h3>
-        <h4>{{ activity.distance }}</h4>
-        <h4>{{ activity.time }}</h4>
+  <ul>
+    <li v-for="(activity, x) in activities" :key="x">
+      <div class="marker" />
+      <div class="debug content">
+        <div class="meta">
+          <h2>{{ activity.date }}</h2>
+          <h3>{{ activity.type }}</h3>
+          <h4>{{ activity.distance }}</h4>
+          <h4>{{ activity.time }}</h4>
+        </div>
         <img :src="`${activity.img}`" />
-      </li>
-    </ul>
-  </div>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <style lang="scss" scoped>
   @import "public/style/grid.scss";
 
+  ul {
+    display: flex;
+    align-items: flex-end;
+    margin: 0 auto;
+    height: 80vh;
+    width: 90vw;
+  }
+
   li {
-    margin-bottom: 12px;
-    border-bottom: 1px solid grey;
+    /* border: 1px solid greenyellow; */
+    position: relative;
+    display: flex;
+    align-items: flex-end;
+    padding: 0 3px;
+    height: 28px;
+    height: 100%;
+  }
+
+  li:hover {
+    .marker {
+      height: 70vh;
+      background: #75f950;
+    }
+
+    .content {
+      width: max-content;
+      opacity: 1;
+    }
+
+    .meta {
+      opacity: 1;
+    }
+
+    img {
+      opacity: 0.5;
+    }
+  }
+
+  .content {
+    border: 1px solid green;
+    position: absolute;
+    top: 0;
+    opacity: 0;
+    text-align: center;
+  }
+
+  .marker {
+    width: 3px;
+    height: 50%;
+    background: red;
+    // transition: 200ms ease all;
+  }
+
+  .meta,
+  img {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: var(--zmin);
+    border-radius: 20px;
+    width: 50vw;
+    opacity: 0;
+  }
+
+  .meta {
+    top: 0;
+    transform: translateY(0%);
   }
 
   img {
@@ -30,21 +97,25 @@
 </style>
 
 <script>
-  import { content, basketball } from "public/content.js"
+  import { basketball, run, hike } from "public/content.js"
   export default {
     data: () => ({
       activities: [
         {
-          date: "Jan 13",
-          type: content.run,
-          img: basketball.bubble,
+          date: "Jan 1",
+          img: basketball.piedmont,
           distance: "",
           time: "45 min",
         },
         {
-          date: "Feb 1",
-          type: content.hike,
-          img: basketball.bubble,
+          date: "Jan 2",
+          img: hike.bubble,
+          distance: "",
+          time: "45 min",
+        },
+        {
+          date: "Jan 3",
+          img: run.tribble,
           distance: "",
           time: "45 min",
         },
--- public/content.js
+++ public/content.js
@@ -1,12 +1,16 @@
-export const content = {
-  basketball: "🏀",
-  run: "🏃‍♂️",
-  hike: "🏔️",
-}
-
 const basketball = {
   icon: "🏀👍",
+  piedmont: "/img/gym@2x.png",
+}
+
+const run = {
+  icon: "🏃‍♂️",
+  tribble: "/img/map@2x.png",
+}
+
+const hike = {
+  icon: "🏔️",
   bubble: "/img/bubble.jpeg",
 }
 
-export { basketball }
+export { basketball, run, hike }
