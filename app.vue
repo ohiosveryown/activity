7ba4664fd41de2ceccf7e937f3c85aedd65ebd1d
ohiosveryown <matt@ohiosveryown.co>
2024-02-22 22:29:58 -0500

    added bg component

--- app.vue
+++ app.vue
@@ -23,23 +23,11 @@
       </li>
     </ul>
   </main>
-  <div class="bg" />
+  <bg />
 </template>
 
 <style lang="scss" scoped>
   @import "public/style/grid.scss";
-
-  .bg {
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: var(--zmin);
-    width: 100vw;
-    height: 100vh;
-    background: url("~/public/img/bg.png") no-repeat center center;
-    background-size: cover;
-    pointer-events: none;
-  }
 
   .progress {
     width: 4px;
---
+++ components/bg.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg" />
+</template>
+
+<style lang="scss" scoped>
+  @import "public/style/grid.scss";
+
+  .bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: var(--zmin);
+    width: 100vw;
+    height: 100vh;
+    background: url("/img/bg.png") no-repeat center center;
+    background-size: cover;
+    pointer-events: none;
+  }
+</style>
